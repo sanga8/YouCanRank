57583752bc3f1d6df881d7d587725a7c7fa62439
Maxence Vercauteren <maxence.vercauteren@isep.fr>
2018-12-18 15:50:03 +0100

    bdd

---
+++ ycr/src/main/java/com/ycr/Model/Categorie.java
@@ -0,0 +1,52 @@
+package com.ycr.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+
+@Entity
+public class Categorie {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    private String type;
+    private String couleur;
+
+	public Integer getId()
+	{
+		return this.id;
+	}
+
+	public void setId(Integer id)
+	{
+		this.id = id;
+	}
+
+	public String getType()
+	{
+		return this.type;
+	}
+
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
+	public String getCouleur()
+	{
+		return this.couleur;
+	}
+
+	public void setCouleur(String couleur)
+	{
+		this.couleur = couleur;
+	}
+
+
+
+    
+}
