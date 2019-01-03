9b8b68c9da7aab0df4302645c6ec1c20d05f4acc
vince <vincentpauwels8@gmail.com>
2019-01-03 19:50:19 +0100

    ok

--- ycr/src/main/java/com/ycr/Controller/HomeController.java
+++ ycr/src/main/java/com/ycr/Controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,32 +14,17 @@ import com.ycr.Model.User;
 import com.ycr.DAO.*;
 
 @Controller
-public class LoginController {
+public class HomeController {
 
-  @Autowired
-  private UserDao userDao;
 
-	@GetMapping(value="/login")
-	public String getLoginForm() {
-		return "login";
+	@GetMapping(value="/")
+	public String Home() {
+		return "index";
 	}
 
-	@RequestMapping(value="/login", method=RequestMethod.POST)
-	public String Login(@ModelAttribute(name="loginForm") User user, Model model) {
-		String pseudo = user.getPseudo();
-		String password = user.getPassword();
-   		String authentification = userDao.authentificationUser(pseudo);
+    @PostMapping(value="  ")
     
-		if (authentification!=null) {
-			if (authentification.equals(password)) {
-				return "index";
-			}
-			else {
-				return "login";
-			}
-		}
-		return "login";
-	}
+	
 	
 
 }
