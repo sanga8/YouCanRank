1d368f168f9802ae8292d8ef1d1268ac26149ff6
gouzmi <guillaume.dupont.rm@gmail.com>
2019-01-07 23:33:35 +0100

    createhtml

--- ycr/src/main/resources/static/accueil.js
+++ ycr/src/main/resources/static/accueil.js
@@ -20,3 +20,14 @@
       document.getElementById("bars").classList.toggle("change");
     }
 }
+
+function hideShow(i) {
+  var x = document.getElementById("q"+i);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+  else  {
+    x.style.display = "none";
+  }
+  
+}
