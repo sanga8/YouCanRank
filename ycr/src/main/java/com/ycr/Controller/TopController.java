22517a7c1b2ecccda0f83d41cb84807473b33e93
vince <vincentpauwels8@gmail.com>
2019-01-09 19:57:25 +0100

    généralisation questions

--- ycr/src/main/java/com/ycr/Controller/TopController.java
+++ ycr/src/main/java/com/ycr/Controller/TopController.java
@@ -33,52 +33,39 @@ public class TopController {
 	public String toCreate(Model model) {
 
 		Top top = new Top();
-		//model.addAttribute("top",top);
 
 		Question q1 = new Question();
-		//model.addAttribute("q1",q1);
+		Question q2 = new Question();
+		Question q3 = new Question();
+		Question q4 = new Question();
+		Question q5 = new Question();
+		Question q6 = new Question();
+		Question q7 = new Question();
+		Question q8 = new Question();
+		Question q9 = new Question();
+		Question q10 = new Question();
+		Question q11 = new Question();
+		Question q12 = new Question();
+		Question q13 = new Question();
 
 		List<Question> questionList= new ArrayList<Question>();
 
 		questionList.add(q1);
+		questionList.add(q2);
+		questionList.add(q3);
+		questionList.add(q4);
+		questionList.add(q5);
+		questionList.add(q6);
+		questionList.add(q7);
+		questionList.add(q8);
+		questionList.add(q9);
+		questionList.add(q10);
+		questionList.add(q11);
+		questionList.add(q12);
+		questionList.add(q13);
 
 		CreateDTO createDTO = new CreateDTO(top, questionList);
 		model.addAttribute("createDTO",createDTO);
-		/*Question q2 = new Question();
-		model.addAttribute("q2",q2);
-
-		Question q3 = new Question();
-		model.addAttribute("q3",q3);
-
-		Question q4 = new Question();
-		model.addAttribute("q4",q4);
-
-		Question q5 = new Question();
-		model.addAttribute("q5",q5);
-
-		Question q6 = new Question();
-		model.addAttribute("q6",q6);
-
-		Question q7 = new Question();
-		model.addAttribute("q7",q7);
-
-		Question q8 = new Question();
-		model.addAttribute("q8",q8);
-
-		Question q9 = new Question();
-		model.addAttribute("q9",q9);
-
-		Question q10 = new Question();
-		model.addAttribute("q10",q10);
-
-		Question q11 = new Question();
-		model.addAttribute("q11",q11);
-
-		Question q12 = new Question();
-		model.addAttribute("q12",q12);
-
-		Question q13 = new Question();
-		model.addAttribute("q13",q13);*/
 
 		return "create";
 	}
@@ -87,53 +74,14 @@ public class TopController {
 	public String Register(CreateDTO createDTO, Model model) {
 
 		topDao.save(createDTO.getTop());
-		//model.addAttribute("top", new Top());
 		
-		
-		createDTO.getQuestionList().get(0).setTop_id(createDTO.getTop().getId());
+		for (int i=0; i<13;++i){
+			if(createDTO.getQuestionList().get(i).getTitre()!=null && createDTO.getQuestionList().get(i).getLink()!=null){
+			createDTO.getQuestionList().get(i).setTop_id(createDTO.getTop().getId());
+			questionRepository.save(createDTO.getQuestionList().get(i));
+			}
+		}
 
-		questionRepository.save(createDTO.getQuestionList().get(0));
-		//model.addAttribute("q1", new Question());
-
-
-
-		/*
-		q2.setTop_id(top.getId());
-		questionRepository.save(q2);
-
-		q3.setTop_id(top.getId());
-		questionRepository.save(q3);
-
-		q4.setTop_id(top.getId());
-		questionRepository.save(q4);
-
-		q5.setTop_id(top.getId());
-		questionRepository.save(q5);
-
-		q6.setTop_id(top.getId());
-		questionRepository.save(q6);
-
-		q7.setTop_id(top.getId());
-		questionRepository.save(q7);
-
-		q8.setTop_id(top.getId());
-		questionRepository.save(q8);
-
-		q9.setTop_id(top.getId());
-		questionRepository.save(q9);
-
-		q10.setTop_id(top.getId());
-		questionRepository.save(q10);
-
-		q11.setTop_id(top.getId());
-		questionRepository.save(q11);
-
-		q12.setTop_id(top.getId());
-		questionRepository.save(q12);
-
-		q13.setTop_id(top.getId());
-		questionRepository.save(q13);
-		*/
 		return "index";
 
 	}
